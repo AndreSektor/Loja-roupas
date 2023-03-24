6f428c64edfff031d3467c16726a29a95b32ff19
AndreSektor <sektor_411@hotmail.com>
2023-03-24 12:03:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <TituloComp />
+  <router-view />
+  <Footercomp />
+  
+</template>
+
+<script>
+
+  import TituloComp from './components/Titulo.vue'
+  // import BlusaView from './views/Blusa.vue'
+  import Footercomp from './components/Footer.vue'
+  export default {
+  
+  name: 'App',
+    components: {
+      TituloComp,
+      // BlusaView,
+      Footercomp
+    }
+  }
+</script>
+
+<style>
+  * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+
+
